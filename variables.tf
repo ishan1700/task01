bdfd28b00c94aa9dada1b6509a9d086ae39b35cc
Ishan Gurao <85431007+ishan1700@users.noreply.github.com>
2024-09-16 09:11:09 +0000

    Elastic Bean Stalk creation

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key"{
-    description="Access key to AWS Console"
-}
-variable "secret_key"{
-    description="Secret key to AWS Console"
-}
-variable "region"{
-    description="AWS Region"
-}
