f9af6a628a0a902440c8a49c05b3579685eaa2a5
Ishan Gurao <85431007+ishan1700@users.noreply.github.com>
2024-09-16 08:29:17 +0000

    some changes

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key"{
+    description="Access key to AWS Console"
+}
+variable "secret_key"{
+    description="Secret key to AWS Console"
+}
+variable "region"{
+    description="AWS Region"
+}
